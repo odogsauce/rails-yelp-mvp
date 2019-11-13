dd8466dfb892166496e3931ea5a7b75e1e6e8070
thibault rahou <trahou33@gmail.com>
2019-11-13 19:01:53 +0100

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create(name: 'lucas Palace', address: '14 rue de la perche', phone_number: '05 43 65 32 12', category: 'italian')
+Restaurant.create(name: 'Kevin cook', address: '4 rue de la gallien', phone_number: '05 46 95 32 22', category: 'chinese')
+Restaurant.create(name: 'Thibaud M&Ms', address: '76 rue de la potter', phone_number: '05 47 00 32 32', category: 'belgian')
+Restaurant.create(name: 'Flo baggle', address: '45 rue du charbon', phone_number: '05 43 65 32 12', category: 'french')
+Restaurant.create(name: 'Paul Dragon', address: '24 rue de la perche', phone_number: '05 43 65 32 12', category: 'japanese')
+
+
+
